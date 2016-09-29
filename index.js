c9437322b48f24c1e46c098fe41931166300715f
dimFora <dimfora@mail.ru>
2016-09-29 14:36:03 +0300

    code: find element

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 /**
  * Created by dimFora on 29.09.2016.
  */
+var inner = document.querySelector('.js-inner');
+alert(inner.innerHTML);
+inner.innerHTML = 'Ворона птица умная';
+inner.style.color = 'red';
--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
 /**
  * Created by dimFora on 29.09.2016.
  */
+
+let inner = document.querySelector('.js-inner') as HTMLElement;
+alert(  inner.innerHTML );
+
+inner.innerHTML = 'Ворона птица умная';
+inner.style.color = 'red';
